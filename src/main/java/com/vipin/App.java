b1148bba662c4f04393551b45b9de908d49c139b
I311636 <vipin.vijaykumar@sap.com>
2019-01-02 11:32:58 +0530

    First

---
+++ src/main/java/com/vipin/App.java
@@ -0,0 +1,38 @@
+package com.vipin;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+
+        System.out.println( "Hello World!" );
+        System.out.println("Started!");
+        HttpServer server = HttpServer.create(new InetSocketAddress(4000), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Java running inside a unikernel!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
